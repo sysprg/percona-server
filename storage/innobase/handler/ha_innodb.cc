6c44adf5d0b7605efb18b3cfe69607c8c7a75c3b
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2015-07-30 18:46:05 +0300

    Re-record binlog.binlog_consistent; switch @@innodb_empty_free_list_algorithm default to legacy until separate LRU flusher is merged from 5.6

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -17683,9 +17683,13 @@ static MYSQL_SYSVAR_ENUM(empty_free_list_algorithm,
   srv_empty_free_list_algorithm,
   PLUGIN_VAR_OPCMDARG,
   "The algorithm to use for empty free list handling.  Allowed values: "
-  "LEGACY: Original Oracle MySQL 5.6 handling with single page flushes; "
-  "BACKOFF: (default) Wait until cleaner produces a free page.",
-  NULL, NULL, SRV_EMPTY_FREE_LIST_BACKOFF,
+  "LEGACY: (default) Original Oracle MySQL 5.6 handling with single page flushes; "
+  "BACKOFF: Wait until cleaner produces a free page.",
+  NULL, NULL, SRV_EMPTY_FREE_LIST_LEGACY,
+  // TODO laurynas: default changed until separate LRU flusher is merged. With
+  // a single page cleaner otherwise it is possible to loop forever in a query
+  // thread while the cleaner is waiting for the page latch held by that
+  // thread. See sys_vars.log_slow_admin_statements_func in 5.7.5.
   &innodb_empty_free_list_algorithm_typelib);
 
 static MYSQL_SYSVAR_ULONG(buffer_pool_instances, srv_buf_pool_instances,
