06a97512a88ff6fbb942d9a6addaa4db82a1f5f3
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2015-10-16 12:48:28 +0300

    Revert bug 1351148 / http://bugs.mysql.com/bug.php?id=73979 fix due to upstream fix in 5.7.6

--- mysys/lf_alloc-pin.c
+++ mysys/lf_alloc-pin.c
@@ -102,13 +102,6 @@
 
 #define LF_PINBOX_MAX_PINS 65536
 
-/*
-  When allocation on stack happens leave this
-  amount of stack memory for further functions
-  call.
-*/
-#define LF_PINBOX_EXTRA_STACK_SPACE  8192
-
 static void lf_pinbox_real_free(LF_PINS *pins);
 
 /*
@@ -342,9 +335,7 @@ static void lf_pinbox_real_free(LF_PINS *pins)
   {
     int alloca_size= sizeof(void *)*LF_PINBOX_PINS*npins;
     /* create a sorted list of pinned addresses, to speed up searches */
-    if (available_stack_size(&pinbox, var->stack_ends_here) >
-        /* leave LF_PINBOX_EXTRA_STACK_SPACE bytes for qsort() call */
-        (alloca_size + LF_PINBOX_EXTRA_STACK_SPACE))
+    if (available_stack_size(&pinbox, var->stack_ends_here) > alloca_size)
     {
       struct st_harvester hv;
       addr= (void **) alloca(alloca_size);
