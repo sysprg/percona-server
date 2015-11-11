bb92534045628055da1c72f4edc091e8f595ece5
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2015-11-11 14:10:30 +0200

    Update test_if_skip_order to set the access type if a cost-based search turns up a better access plan than JT_ALL. This fixes a regression in main.order_by_limit. Update that testcase for the covering index preference in PS

--- sql/sql_optimizer.cc
+++ sql/sql_optimizer.cc
@@ -2160,6 +2160,8 @@ test_if_skip_sort_order(JOIN_TAB *tab, ORDER *order, ha_rows select_limit,
       */
       DBUG_ASSERT(tab->quick() == save_quick || tab->quick() == NULL);
       tab->set_quick(qck);
+      if (qck)
+        tab->set_type(calc_join_type(qck->get_type()));
     }
     order_direction= best_key_direction;
     /*
